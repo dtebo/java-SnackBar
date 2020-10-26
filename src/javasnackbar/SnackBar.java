02cf03cfb045d5813d988155f50904a15d4cee2f
Darren Tebo <darrentebo83@outlook.com>
2020-10-26 17:03:03 -0400

    began implementing required output

--- src/javasnackbar/SnackBar.java
+++ src/javasnackbar/SnackBar.java
@@ -14,8 +14,23 @@ public class SnackBar{
         Snack water = new Snack("Water", 20, 2.75, v2.getId());
 
         Snack gum = new Snack("Gum", 10, 0.75, v3.getId());
-        
+
         Customer c1 = new Customer("Jane", 45.25);
         Customer c2 = new Customer("Bob", 33.14);
+
+        c1.buySnacks(3 * soda.getCost());
+
+        System.out.println("Jane cash on hand " + c1.getCashOnHand());
+        System.out.println("Quantity of Soda " + soda.getQuantity());
+
+        c1.buySnacks(1 * pretzels.getCost());
+
+        System.out.println("Jane cash on hand " + c1.getCashOnHand());
+        System.out.println("Quantity of Pretzel " + pretzels.getQuantity());
+
+        c2.buySnacks(2 * soda.getCost());
+
+        System.out.println("Bob cash on hand " + c2.getCashOnHand());
+        
     }
 }
